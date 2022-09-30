a73d916f0f096b4d8f140925539ee5ad24834fff
Denis Maina <Denis.maina@student.moringaschool.com>
2022-09-30 18:26:24 +0300

    commit tree 3b2cdaffb114b7d946bc41517cb178bd6ffa9282

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = get_user
+      items = uer.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+  def show
+    item = Item.find_by(id:params[:id])
+    if item
+      render json: item
+  end
+
+  def create
+    user = get_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def get_user
+    User.find(params[:user_id])
+  end
+
 
 end
